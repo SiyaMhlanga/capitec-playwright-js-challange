84dc968ccc10bb5fe53f9930c54c35b4d99f37b2
Siyabonga Mhlanga <siyabonga.mhlanga@rank.com>
2025-06-03 15:47:21 +0200

    Format code

--- tests/ui/inventory.test.js
+++ tests/ui/inventory.test.js
@@ -1,29 +1,24 @@
-import { test,expect} from '@playwright/test';
-import { LoginPage } from '../../src/page-objects/login.page.js';
-import { InventoryPage } from '../../src/page-objects/inventory.page.js';
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../../src/page-objects/login.page.js";
+import { InventoryPage } from "../../src/page-objects/inventory.page.js";
+import uiCredentials from "../../test-data/uiCredentials.json" assert { type: "json" };
 
-import uiCredentials from '../../test-data/uiCredentials.json' assert {type : 'json'};
-
-test.describe.serial('UI Invetory Test Flow', () => {
-
-    test.beforeEach(async ({page}) =>{
-
-        const loginPage = new  LoginPage(page);
-        const inventoryPage = new InventoryPage(page);
-
-        const user = uiCredentials.validUsers.find(x => x.username =='standard_user');
-        await loginPage.goto();
-        await loginPage.login(user.username,user.password);
-
-        await inventoryPage.expectOnInventoryPage();
-
-    });
-    test(' Verify the total number of items in the inventory page', async ({page}) => {
-
-        const inventoryPage = new InventoryPage(page)
-        const count = await inventoryPage.getItemCount();
-        await expect(count).toBe(6);
-
-
-    });
+test.describe.serial("UI Invetory Test Flow", () => {
+  test.beforeEach(async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    const inventoryPage = new InventoryPage(page);
+    const user = uiCredentials.validUsers.find(
+      (x) => x.username == "standard_user"
+    );
+    await loginPage.goto();
+    await loginPage.login(user.username, user.password);
+    await inventoryPage.expectOnInventoryPage();
+  });
+  test(" Verify the total number of items in the inventory page", async ({
+    page,
+  }) => {
+    const inventoryPage = new InventoryPage(page);
+    const count = await inventoryPage.getItemCount();
+    await expect(count).toBe(6);
+  });
 });
